80b8ce1dbed74acd370611c8ea1e4a2fdf56e77e
phuparu <phumiphat.140@gmail.com>
2024-04-08 21:03:51 +0700

    Set Person Routes

--- MentalProj/src/App.jsx
+++ MentalProj/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Person1 from './pages/person1'
-
+import Person2 from './pages/person2'
+import Person3 from './pages/person3'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Person1 />} />
+          <Route path='/person1' element={<Person1 />} />
+          <Route path='/person2' element={<Person2 />} />
+          <Route path='/person3' element={<Person3 />} />
         </Routes>
       </BrowserRouter>
     </>
