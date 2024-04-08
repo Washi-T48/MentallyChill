207c1a791a1eb61c00b2293b95863d7f80c914f8
phuparu <phumiphat.140@gmail.com>
2024-04-08 20:43:28 +0700

    Update Person1
    Adjust profile pic + Add detail

--- MentalProj/src/pages/Person1.jsx
+++ MentalProj/src/pages/Person1.jsx
@@ -1,25 +1,29 @@
 import "./person1.css";
 import React from "react";
+import CRA01 from "../images/CRA01.jpg";
+import CRAlogo from "../images/CRAlogo.png";
 export default function Person1() {
     
     return (
         <div className='page'>
     
           <div className='content'>
-            <img className='logo' src={"https://s3-alpha-sig.figma.com/img/bcc7/a7af/0dde4ea90327ba44fc451a20ba3e13e4?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Q90v14B14E-m9pH8Wy3iq~lSnrC~bz7auFDbCQpXnyZRK1IADMR-Gnla8S4-53aFJoyCEt0S-yLyykBBoO4v5YlbFPC6nTmFQ9-7~p1dpYy3CFmOrNJz0xeOOAsE6uFDDGsdn1BU2PqXjvhkHynoSjjpdSwhTuW~zdy3QDOhOgw87fLItgI8qw53l4glHDaaLulgKAZAhxWWsfQPbIc~mfOJNljOqH5K-KqGdpUTrRIlgcXKRGY85f5BBuEYM-LfJLkiMcn7-YuSFvhXXfzkQytAhaTDA1IwWUg~6Z-0SE2V~87iIHlhqJb-m2WdTcMIkLOkVJNxejPVXl2phaLv5A__" }>
+            <img className='logo' src={CRAlogo}>
             </img>
-            <p>Psychiatry is the branch of medicine focused on the diagnosis, treatment and 
-              prevention of mental, emotional and 
-              behavioral disorders.</p>
+            <p>ปริญญาเอก : Nursing Science School of Nursing, Case Western Reserve University, USA<br/>
+            <br/>
+            ปริญญาโท : ศิลปศาสตรมหาบัณฑิต สาขาจิตวิทยาการปรึกษา คณะครุศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย<br/>
+            <br/>
+            ปริญญาตรี : วิทยาศาสตร์บัณฑิต(พยาบาลและผดุงครรภ์) คณะพยาบาลศาสตร์ มหาวิทยาลัยมหิดล<br/> </p>
     
-            <p>A psychiatrist is a medical doctor (an M.D. or D.O.) who specializes in mental health, including substance use disorders. Psychiatrists are qualified to assess both the mental and physical aspects of psychological problems.</p>
+            {/*<p>A psychiatrist is a medical doctor (an M.D. or D.O.) who specializes in mental health, including substance use disorders. Psychiatrists are qualified to assess both the mental and physical aspects of psychological problems.</p>*/}
           </div>
     
           <div className='footer'>
-            <img src={'https://s3-alpha-sig.figma.com/img/c45e/85e1/78839b2115f553d3f6924de788b265c3?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nysEZwVxLxEPXya2aaWJtJ2XnYu8W3kCm~9hmIwm8fxYFk~F8A4xLegmtOe5OJ7~Df3JbXXPAvgWvnDKMW9BdII-jZezWphcZccy0Ha0GnJGD7svViybKAy1mj2iQtV~rMWEFLJRkxcN77e6CJ~W5Uv3niBOsvzMu5SWYpS0fWvnlgci1H0x-LPSFUmAvfTyS9-dbweaYoUkfc2BoRgdBkfMvLbV0T3kKkYfRO0e-9NrPDHsFnn6FXSGy6GPNQQzazOxEV5vNTJqbElR9Jm8D5B05GGsaFNaqFxeWcz7dtRDnlWoIeHiXYUfdxPmEPPD11CaU6sQVAkxnLnmGBLWwQ__'}></img>
+            <img src={CRA01}></img>
             <div className="profile">
-              <h1>Name</h1>
-              <p>text</p>
+              <h1>รหัส CRA01 รุ้งนภา ผาณิตรัตน์ </h1>
+              <p>(พี่รุ้ง)</p>
             </div>
           </div>
     
