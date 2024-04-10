e0277d81354c78b2597434322b8ce0ab6b651e67
phuparu <phumiphat.140@gmail.com>
2024-04-10 18:23:28 +0700

    remark2

---
+++ Frontend/src/pages/remark2.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./remark2.css"
+import CRAlogo from "../images/CRAlogo.png";
+export default function Remark2() {
+  return (
+    <div>
+      <div className='logo'>
+        <img className='logo' src={CRAlogo}></img>
+      </div>
+      <div className='remark2-content'>
+      &nbsp;&nbsp;&nbsp;&nbsp;เครื่องมือนี้ ใช้ในการคัดกรอง ไม่ใช่เครื่องมือที่ใช้ในการวินิจฉัยโรค กล่าวคือ ระดับคะแนนที่สูงจะช่วยบ่งชี้ว่าคุณอาจจะมีคุณภาพชีวิตที่ไม่ดีและภาวะแปรปรวนทางอารมณ์ อย่างไรก็ตาม แบบประเมินนี้ไม่สามารถวินิจฉัยโรคทางจิตเวชหรือสาเหตุของภาวะอารมณ์เชิงลบที่คุณประสบอยู่ได้
+	<br/>&nbsp;&nbsp;&nbsp;&nbsp;แบบประเมินสุขภาพจิตเป็นการประเมินด้วยตัวเอง คุณสามารถทำเมื่อใดก็ได้ ให้เลือกข้อที่คิดว่าตรงกับท่านมากที่สุด เพื่อวัดระดับภาวะซึมเศร้า ความวิตกกังวล และความเครียด ถ้าได้ผลแตกต่างกันในแต่ละครั้งถือเป็นเรื่องผิดปกติ
+
+      </div>
+      <div className='remark2-footer'>
+        <button className='btn btn-acc' onClick={<></>}>Accept</button>
+      </div>
+    </div>
+  )
+}
