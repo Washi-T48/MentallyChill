c1120ef82e2dd829a92e43cbd459845fe1ffae47
phuparu <100335395+phuparu@users.noreply.github.com>
2024-04-22 22:02:34 +0700

    Layout

--- Frontend/src/pages/p1_dass21.jsx
+++ Frontend/src/pages/p1_dass21.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./p1_dass21.css"
+import CRAlogo from "../images/CRAlogo.png";
 export default function P1_dass21() {
   return (
     <div>
-      
+      <div className='logo'>
+        <img className='logo' src={CRAlogo}></img>
+      </div>
+      <div className='p1_dass21-content'>
+
+      </div>
     </div>
   )
 }
