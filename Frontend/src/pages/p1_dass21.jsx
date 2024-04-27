be640679737aaf45c6e8fabdfd4f584ee404cfe5
phuparu <phumiphat.140@gmail.com>
2024-04-27 15:54:39 +0700

    create logo component

--- Frontend/src/pages/p1_dass21.jsx
+++ Frontend/src/pages/p1_dass21.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './p1_dass21.css'
-import CRAlogo from "../images/CRAlogo.png";
+import Logo from './logo';
 export default function P1_dass21() {
   return (
     <div>
-      <div className='logo'>
-        <img className='logo' src={CRAlogo}></img>
+      <Logo/>
+      <div className='p1_dass21-content'>
+      โปรดอ่านแต่ละข้อความและเลือกตัวเลข 0, 1, 2 หรือ 3 ซึ ่งระบุว่าค ากล่าวนั้นตรงกับคุณมากแค่ไหนในชวงสปดาหทผานมา ไม่มีค าตอบที ่ถูกหรือผิด 
+อย่าใช้เวลามากเกินไปกับข้อความใด ๆ
+
       </div>
     </div>
   )
