6963646c1bf1983b3368fd6c0b4359080724dbf2
phuparu <phumiphat.140@gmail.com>
2024-05-01 17:26:10 +0700

    dass21 Update

--- Frontend/src/pages/cri_dass21.jsx
+++ Frontend/src/pages/cri_dass21.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./cri_dass21.css"
 import Logo from './logo';
+import P3_dass21 from './p3_dass21';
+import { Link } from 'react-router-dom';
 export default function CRI_dass21() {
 
   const navigateToPreviousPage = () => {
@@ -26,46 +28,54 @@ export default function CRI_dass21() {
                   3         หมายถึง          ตรงกับข้าพเจ้ามาก หรือเกิดขึ้นบ่อยมากที่สุด<br/>
             </p>
             <table>
-              <tr>
-                <th></th>
-                <th>อาการซึมเศร้า (D)  </th>
-                <th>ความวิตกกังวล (A)  </th>
-                <th>ความเครียด (S)  </th>
-              </tr>
-              <tr>
-                <td>ปกติ</td>
-                <td>0-4</td>
-                <td>0-3</td>
-                <td>0-7</td>
-              </tr>
-              <tr>
-                <td>ระดับต่ำ</td>
-                <td>5-6</td>
-                <td>4-5</td>
-                <td>8-9</td>
-              </tr>
-              <tr>
-                <td>ระดับปานกลาง</td>
-                <td>7-10</td>
-                <td>6-7</td>
-                <td>10-12</td>
-              </tr>
-              <tr>
-                <td>ระดับรุนแรง</td>
-                <td>11-13</td>
-                <td>8-9</td>
-                <td>13-16</td>
-              </tr>
-              <tr>
-                <td>ระดับรุนแรงที่สุด</td>
-                <td>14 ขึ้นไป</td>
-                <td>10 ขึ้นไป</td>
-                <td>17 ขึ้นไป</td>
-              </tr>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>อาการซึมเศร้า (D)  </th>
+                  <th>ความวิตกกังวล (A)  </th>
+                  <th>ความเครียด (S)  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>ปกติ</td>
+                  <td>0-4</td>
+                  <td>0-3</td>
+                  <td>0-7</td>
+                </tr>
+                <tr>
+                  <td>ระดับต่ำ</td>
+                  <td>5-6</td>
+                  <td>4-5</td>
+                  <td>8-9</td>
+                </tr>
+                <tr>
+                  <td>ระดับปานกลาง</td>
+                  <td>7-10</td>
+                  <td>6-7</td>
+                  <td>10-12</td>
+                </tr>
+                <tr>
+                  <td>ระดับรุนแรง</td>
+                  <td>11-13</td>
+                  <td>8-9</td>
+                  <td>13-16</td>
+                </tr>
+                <tr>
+                  <td>ระดับรุนแรงที่สุด</td>
+                  <td>14 ขึ้นไป</td>
+                  <td>10 ขึ้นไป</td>
+                  <td>17 ขึ้นไป</td>
+                </tr>
+              </tbody>
             </table>
           </div>
       </div>
-      
+      <div className='cri_dass21-footer'>
+          <Link to="/p3_dass21">
+            <button className='btn btn-result'>See the result</button>
+          </Link>
+        </div>
     </div>
   )
 }
