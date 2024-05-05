3246d7319c3671db336c1fb8c57e6179e9c063e6
phuparu <100335395+phuparu@users.noreply.github.com>
2024-05-05 22:10:15 +0700

    Layout

--- Frontend/src/pages/appoint.jsx
+++ Frontend/src/pages/appoint.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import "./appoint.css"
 import Logo from './logo'
+import { Link } from 'react-router-dom'
+
 export default function Appoint() {
     const [appointData, setAppointData] = useState({
         tel:"",
@@ -26,7 +28,9 @@ export default function Appoint() {
                         className='app-tel' 
                         type='tel'
                         placeholder='0000 000 0000'
+                        value={appointData.tel}
                         name='tel'
+                        required
                         >
                     </input>
                     </div>
@@ -49,10 +53,64 @@ export default function Appoint() {
                             name='tel'
                             >
                         </input>
-                        <span class="checkmark"></span>
+                        <span className="checkmark"></span>
+                    </label>
+                </div>
+                <div className="app-advisor">
+                    <label>เลือกผู้ให้คำปรึกษา
+                        <select>
+                            <option value="CRA01">รุ้งนภา ผาณิตรัตน์ (พี่รุ้ง)</option>
+                            <option value="CRA02">ดวงแก้ว เตชะกาญจนเวช (พี่ปู)</option>
+                            <option value="CRA03">วิภาพร สร้อยแสง (พี่อ้อย)</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="app-time">
+                    <label>วันที่และเวลา<br/>
+                        <input type="date" required="required"/><br/>
+                        <select>
+                            <option value={"/"}>TIME-1</option>
+                            <option value={"/"}>TIME-1</option>
+                            <option value={"/"}>TIME-1</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="app-topic">
+                    <label>เรื่องที่ต้องการปรึกษา<br/>
+                        <select>
+                                <option value={"/"}>TOPIC-1</option>
+                                <option value={"/"}>TOPIC-2</option>
+                                <option value={"/"}>TOPIC-3</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="app-detail">
+                    <label>เรื่องที่ขอรับการปรึกษา
+                        <input 
+                            className='app-detail' 
+                            type='text'
+                            name='detail'
+                            >
+                        </input>
+                    </label>
+                </div>
+                <div className="med-his">
+                    <label>ประวัติการรับยา
+                        <input 
+                            className='med-his' 
+                            type='text'
+                            name='med_history'
+                            >
+                        </input>
                     </label>
                 </div>
             </form>
+
+            <div className='dass21-app-footer'>
+            <Link to="/">
+                <button className='btn btn-next'>จองเลย</button>
+            </Link>
+            </div>
         </div>
     </div>
   )
