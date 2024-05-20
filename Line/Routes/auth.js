07684039400a6e8bc4e54559048c1a958b6907c7
Pichayut Sombat <psb.nnt@gmail.com>
2024-05-20 12:10:22 +0700

    minor edit

--- Line/Routes/auth.js
+++ Line/Routes/auth.js
@@ -3,23 +3,6 @@ import dotenv from "dotenv";
 import querystring from "querystring";
 dotenv.config();
 
-// export async function getAccessToken() {
-//     const response = await axios.post('https://api.line.me/oauth2/v3/token', querystring.stringify({
-//         grant_type: 'client_credentials',
-//         client_id: process.env.CHANNEL_ID,
-//         client_secret: process.env.CHANNEL_SECRET
-//     }), {
-//         headers: {
-//             contentType: 'application/x-www-form-urlencoded'
-//         }
-//     });
-//     return response.data.access_token;
-// }
-
-// export function getAccessToken() {
-//     return process.env.CHANNEL_SECRET;
-// }
-
 export function getAuthHeader() {
     return {
         'Content-Type': 'application/json',
