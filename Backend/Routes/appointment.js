bf95ab1e0f84600032e5f51dc452c62e2db29e19
Pichayut Sombat <psb.nnt@gmail.com>
2024-05-26 16:28:59 +0700

    appointment.js finally worked

--- Backend/Routes/appointment.js
+++ Backend/Routes/appointment.js
@@ -7,29 +7,31 @@ dotenv.config();
 
 const appointmentRouter = express.Router();
 
-appointmentRouter.post("/appoint", async (req, res) => {
-    const { tel,
-        uuid,
+appointmentRouter.post("/newAppointment", async (req, res) => {
+    const {
+        uid,
+        tel,
         contactMethod,
         medDoctor,
-        datetime,
         date,
         time,
         topic,
         detail,
-        medHistory
+        medHistory,
     } = req.body;
 
     try {
         console.log(req.body);
         const newAppointment = await pool.query(
-            `INSERT INTO appointment (user_id, contact, contact_method, staff_id, appointment_date, topic, medHistory) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
-            [uuid, tel, contactMethod, medDoctor, topic, medHistory]
+            `INSERT INTO appointment (user_id, contact, contact_method, staff_id, appointment_date, topic, details, medical_history) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
+            [uid, tel, contactMethod, medDoctor, String(date) + " " + String(time), topic, detail, medHistory]
         );
         console.log(newAppointment);
-        res.status(200).json(newAppointment);
+        res.status(200).json(newAppointment["rows"][0]);
+        // res.send([uid, tel, contactMethod, medDoctor, String(date) + " " + String(time), topic, detail, medHistory]);
     } catch (err) {
         logger.error(err.message);
+        res.sendStatus(500);
     }
 });
 
