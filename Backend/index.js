c122a66682737c331c56456892576dfb1a7f9120
Pichayut Sombat <psb.nnt@gmail.com>
2024-05-27 23:35:57 +0700

    use cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 
@@ -10,6 +11,7 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
