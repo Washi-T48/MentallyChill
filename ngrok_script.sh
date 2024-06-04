5fbeb606c7fd86ac238e8da44cd606ca70ffeb4d
Pichayut Sombat <psb.nnt@gmail.com>
2024-06-05 01:25:19 +0700

    Update ngrok_script.sh

--- ngrok_script.sh
+++ ngrok_script.sh
@@ -1,3 +1,3 @@
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 ngrok config add-authtoken <token>
-ngrok http --scheme=https 80 --domain=<domain>
+ngrok http --scheme=https --domain=<domain> 5173
