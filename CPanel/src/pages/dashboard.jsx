0458d4e3d0cc61b0971ac349b9a9721c6eac5ac3
martksk <kritsakorn.lekd@kmutt.ac.th>
2024-06-29 02:54:18 +0700

    test topbar

--- CPanel/src/pages/dashboard.jsx
+++ CPanel/src/pages/dashboard.jsx
@@ -138,7 +138,7 @@ export default function DashboardPage() {
 
   return (
     <>
-      <div className="flex flex-col flex-1 h-dvh">
+      <div className="flex flex-col flex-1 h-dvh w-80">
         <Topbar />
         <div className="flex flex-row flex-1">
           <div className="flex relative w-72">
