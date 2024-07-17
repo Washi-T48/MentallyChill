e54a33ad3d572f31f3775aab8504d0d13981e207
Pichayut Sombat <psb.nnt@gmail.com>
2024-07-17 23:27:22 +0700

    un-bypass auth.js

--- Backend/Middleware/auth.js
+++ Backend/Middleware/auth.js
@@ -5,18 +5,17 @@ import logger from './logger.js';
 dotenv.config();
 
 const authMiddleware = (req, res, next) => {
-    // const token = req.cookies.token;
-    // if (!token) { return res.sendStatus(401) }
+    const token = req.cookies.token;
+    if (!token) { return res.sendStatus(401) }
 
-    // try {
-    //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    //     req.user = decoded;
-    //     next();
-    // } catch (err) {
-    //     logger.error(err);
-    //     return res.sendStatus(401);
-    // }
-    next();
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        logger.error(err);
+        return res.sendStatus(401);
+    }
 };
 
 export default authMiddleware;
