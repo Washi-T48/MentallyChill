5ae3ffe1b91d5e9ba8404656ada3d01eab3ff224
Pichayut Sombat <psb.nnt@gmail.com>
2024-07-20 16:10:02 +0700

    update cpanel configs to ssl

--- CPanel/vite.config.js
+++ CPanel/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from 'fs'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "http://ligma.sombat.cc:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../certs', 'privkey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../certs', 'cert.pem')),
+      ca: fs.readFileSync(path.resolve(__dirname, '../certs', 'chain.pem')),
     },
+    host: '0.0.0.0',
+    port: 443,
   },
 });
