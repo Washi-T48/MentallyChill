c66f04813b96eb1aea5d192925310d4449fee634
Pichayut Sombat <psb.nnt@gmail.com>
2024-07-20 15:23:52 +0700

    update vite config for ssl

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +9,14 @@ export default defineConfig({
   optimizeDeps: {
     include: ['@emotion/styled', '@emotion/react'],
   },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs', 'privkey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'cert.pem')),
+      ca: fs.readFileSync(path.resolve(__dirname, 'certs', 'ca.pem')),
+    },
+    host: '0.0.0.0',
+    port: 443,
+  }
+
 });
