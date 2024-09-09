efc85a3c6e1f88f462a56afb21d6dd52c9e5d1b1
Pichayut Sombat <psb.nnt@gmail.com>
2024-09-09 09:03:00 +0700

    edit sub_topic case

--- Backend/Models/appointment.js
+++ Backend/Models/appointment.js
@@ -1,10 +1,10 @@
 import pool from "../Config/db.js";
 
-const newAppointment = async (uid, tel, contactMethod, medDoctor, date, time, topic, detail, medHistory, sub_topic) => {
+const newAppointment = async (uid, tel, contactMethod, medDoctor, date, time, topic, detail, medHistory, subtopic) => {
     const datetime = String(date) + " " + String(time);
     const newAppointment = await pool.query(
         `INSERT INTO appointment (user_id, contact, contact_method, staff_id, appointment_date, topic, details, medical_history, sub_topic) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-        [uid, tel, contactMethod, medDoctor, datetime, topic, detail, medHistory, sub_topic]
+        [uid, tel, contactMethod, medDoctor, datetime, topic, detail, medHistory, subtopic]
     );
     return (newAppointment["rows"][0]);
 };
@@ -89,7 +89,7 @@ const postAppointment = async (booking_id, status, post_note, post_feedback, pos
 const submitAppointment = async (uid, tel, contactMethod, medDoctor, date, time, topic, detail, medHistory, subtopic) => {
     const newAppointment = await pool.query(
         `INSERT INTO appointment (user_id, contact, contact_method, staff_id, appointment_date, topic, details, medical_history, sub_topic) VALUES ((SELECT user_id FROM users WHERE line_uid = $1), $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-        [uid, tel, contactMethod, medDoctor, String(date) + " " + String(time), topic, detail, medHistory, sub_topic]
+        [uid, tel, contactMethod, medDoctor, String(date) + " " + String(time), topic, detail, medHistory, subtopic]
     );
     return (newAppointment["rows"][0]);
 }
