eccdfe1d080acbb8ee8f6f85ee8da00766175ba3
Pichayut Sombat <psb.nnt@gmail.com>
2024-09-09 19:39:46 +0700

    Revert "Merge branch 'main' of https://github.com/Washi-T48/MentallyChill"
    This reverts commit d02865f7b886abcf220bd0be51f77811cb8b8fe0, reversing
    changes made to 60d06eba5bc3e94e4f4cb19f1c035e8af05cfce4.

--- Backend/Routes/appointmentRoutes.js
+++ Backend/Routes/appointmentRoutes.js
@@ -60,6 +60,7 @@ appointmentRouter.put("/update", async (req, res) => {
         const { booking_id, date, time, topic, detail, medHistory } = req.body;
         const appointment = await updateAppointment(booking_id, date, time, topic, detail, medHistory);
         res.status(200).json(appointment);
+        await appointmentNotify(appointment.booking_id);
     }
     catch (error) {
         logger.error(error);
