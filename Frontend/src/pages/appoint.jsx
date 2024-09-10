114d617614e935da599aff686a6d93570ed8bd54
BankDesu <psuwimonopas@gmail.com>
2024-09-10 17:15:05 +0700

    Update appoint.jsx

--- Frontend/src/pages/appoint.jsx
+++ Frontend/src/pages/appoint.jsx
@@ -53,7 +53,8 @@ export default function Appoint() {
   const [loading, setLoading] = useState(false);
   const [currentDate, setCurrentDate] = useState("");
   const [error, setError] = useState("");
-  const [staffList, setStaffList] = useState([]);
+  const [allStaff, setAllStaff] = useState([]);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -79,28 +80,30 @@ export default function Appoint() {
     const today = new Date();
     const formattedDate = today.toISOString().split("T")[0];
     setCurrentDate(formattedDate);
-
-    axios.get("/allStaff")
-    .then((response) => {
-      if (Array.isArray(response.data)) {
-        setStaffList(response.data);
-      } else {
-        console.error("Unexpected response format:", response.data);
-        setStaffList([]);
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching staff data:", error);
-      setStaffList([]);
-    });
   }, []);
-
 
   useEffect(() => {
     if (appointData.date && appointData.medDoctor) {
       fetchAvailableTimeSlots();
     }
   }, [appointData.date, appointData.medDoctor]);
+
+  useEffect(() => {
+    const fetchStaffData = async () => {
+      try {
+        const response = await axios.get(
+          `/allStaff`
+        );
+        setAllStaff(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchStaffData();
+  }, []);
+
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -136,6 +139,7 @@ export default function Appoint() {
     });
   };
 
+
   const handleRadioSelect = (name, value) => {
     setAppointData((prevData) => ({
       ...prevData,
@@ -152,18 +156,10 @@ export default function Appoint() {
 
   const fetchAvailableTimeSlots = () => {
     setLoadingSlots(true);
-    axios.post("/getStaffTimeByDate", {
-      staff_id: appointData.medDoctor,
-      date: appointData.date,
-    })
-    .then((response) => {
-      setTimeSlots(response.data);
+    setTimeout(() => {
+      setTimeSlots(["09:00", "10:00", "11:00", "13:00", "14:00"]);
       setLoadingSlots(false);
-    })
-    .catch((error) => {
-      console.error("Error fetching available time slots:", error);
-      setLoadingSlots(false);
-    });
+    }, 1000);
   };
 
   const hasSubtopics = (topic) => topics[topic]?.length > 0;
@@ -237,6 +233,21 @@ export default function Appoint() {
             <label>
               เลือกผู้ให้คำปรึกษา<mark> *</mark>
               <br />
+              {/* <select
+                name="medDoctor"
+                value={appointData.medDoctor}
+                onChange={handleChange}
+                required
+              >
+                <option value="">เลือกผู้ให้คำปรึกษา</option>
+                <option value="CRA01">
+                  CRA01 รุ้งนภา ผาณิตรัตน์ (พี่รุ้ง)
+                </option>
+                <option value="CRA02">
+                  CRA02 ดวงแก้ว เตชะกาญจนเวช (พี่ปู)
+                </option>
+                <option value="CRA03">CRA03 วิภาพร สร้อยแสง (พี่อ้อย)</option>
+              </select> */}
               <select
                 name="medDoctor"
                 value={appointData.medDoctor}
@@ -244,7 +255,7 @@ export default function Appoint() {
                 required
               >
                 <option value="">เลือกผู้ให้คำปรึกษา</option>
-                {staffList.map((staff) => (
+                {allStaff.map((staff) => (
                   <option key={staff.staff_id} value={staff.staff_id}>
                     {`${staff.staff_id} ${staff.name} ${staff.surname} (${staff.nickname})`}
                   </option>
@@ -271,23 +282,20 @@ export default function Appoint() {
                 onChange={handleChange}
                 required
                 disabled={
-                  !appointData.date || loadingSlots || timeSlots.length === 0
+                  !appointData.date || !appointData.medDoctor || loadingSlots
                 }
               >
                 <option value="">เลือกเวลา</option>
-                {loadingSlots ? (
-                  <option>Loading slots...</option>
-                ) : (
-                  timeSlots.map((timeSlot) => (
-                    <option key={timeSlot} value={timeSlot}>
-                      {timeSlot}
-                    </option>
-                  ))
-                )}
+                {timeSlots.map((slot) => (
+                  <option key={slot} value={slot}>
+                    {slot}
+                  </option>
+                ))}
               </select>
+              {loadingSlots && <p>Loading available time slots...</p>}
             </label>
           </div>
-          <div className="app-detail">
+          <div className="app-topic">
             <label>
               ประเด็นที่ต้องการปรึกษา<mark> *</mark>
               <br />
