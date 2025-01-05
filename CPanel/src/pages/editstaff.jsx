77a655d7826d2e9cc09423b570425f56b299c4cd
martksk <kritsakorn.lekd@kmutt.ac.th>
2025-01-05 14:17:50 +0700

    Update editstaff.jsx

--- CPanel/src/pages/editstaff.jsx
+++ CPanel/src/pages/editstaff.jsx
@@ -1,15 +1,24 @@
+import axios from "../components/axioscreds";
+import { useEffect, useState } from "react";
 import Sidebar from "../components/sidebar";
 import Topbar from "../components/topbar";
 
 export default function EditStaffPage() {
 
 return (
-    <>
-      <Sidebar />
-      <Topbar />
-      <div className="content">
-        <h1>Edit Staff</h1>
+  <div className="flex flex-col min-h-screen">
+    <Topbar />
+    <div className="flex flex-1 flex-row">
+      <div className="flex relative w-72">
+        <Sidebar />
       </div>
-    </>
-  );
+      <div className="w-full overflow-x-hidden">
+        <div className="p-4 md:p-10">
+            แก้ไขข้อมูลพนักงาน
+        </div>
+      </div>
+    </div>
+  </div>
+);
 }
+
