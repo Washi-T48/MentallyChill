b7fd81daddf2470677b70cea81bea1406768ae2b
Pichayut Sombat <psb.nnt@gmail.com>
2025-01-06 21:06:57 +0700

    add specific admin middleware

--- Backend/Middleware/auth.js
+++ Backend/Middleware/auth.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import logger from './logger.js';
+import { getPermission } from '../Models/auth.js';
 
 dotenv.config();
 
@@ -12,6 +13,23 @@ const authMiddleware = (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
+    } catch (err) {
+        logger.error(err);
+        return res.sendStatus(401);
+    }
+    // next();
+};
+
+const adminMiddleware = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) { return res.sendStatus(401) }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+
+        const permission = (await getPermission(decoded.staff_id)).permission;
+        if (permission !== 'admin') { return res.sendStatus(401) }
     } catch (err) {
         logger.error(err);
         return res.sendStatus(401);
