64fd0fd5da6491bef6e24e8375142a32d4fb70c8
martksk <kritsakorn.lekd@kmutt.ac.th>
2025-01-08 13:12:12 +0700

    Update topbar.jsx

--- CPanel/src/components/topbar.jsx
+++ CPanel/src/components/topbar.jsx
@@ -19,7 +19,7 @@ export default function Topbar() {
         console.error("Error fetching data:", error);
       }
     };
-    console.log(staff.staff_id);
+    console.log(staff);
 
     fetchStaffData();
   }, []);
