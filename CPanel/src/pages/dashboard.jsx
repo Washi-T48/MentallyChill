439278b070b947b1316a2f0fb9f26c7499e0e8bc
martksk <krtsk2547@gmail.com>
2025-02-20 14:14:48 +0700

    Update dashboard.jsx

--- CPanel/src/pages/dashboard.jsx
+++ CPanel/src/pages/dashboard.jsx
@@ -208,7 +208,7 @@ export default function DashboardPage() {
               />
               <StatCard title="ระดับร้ายแรง" value={highCount} color="rose" />
             </div>
-            <div className="border-4 border-blue rounded-md p-4">
+            <div className="border-4 border-blue-700 bg-white rounded-md p-4">
               <CategoryStats
                 title="ความซึมเศร้า"
                 low={countdlow}
