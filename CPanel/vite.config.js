1dba95a6a0ca26de63b1b13e3d97c767de7564c3
Pichayut Sombat <psb.nnt@gmail.com>
2025-03-21 16:09:48 +0700

    change all to http
    use nginx instead

--- CPanel/vite.config.js
+++ CPanel/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,19 +7,6 @@ export default defineConfig({
   server: {
     open: true,
     host: '0.0.0.0',
-    port: 443,
-    https: {
-      key: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/privkey.pem`),
-      cert: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/cert.pem`),
-      ca: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/chain.pem`),
-    },
-    proxy: {
-      '/api': {
-        target: 'https://sombat.cc:3000/',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
+    port: 444,
   },
 });
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,19 +11,6 @@ export default defineConfig({
   server: {
     open: true,
     host: '0.0.0.0',
-    port: 443,
-    https: {
-      key: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/privkey.pem`),
-      cert: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/cert.pem`),
-      ca: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/chain.pem`),
-    },
-    proxy: {
-      '/api': {
-        target: 'https://sombat.cc:3000/',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
+    port: 8000,
   }
 });
