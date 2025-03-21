39baedb28cc9a80f9d7fafb33b853e2b00b39818
Pichayut Sombat <psb.nnt@gmail.com>
2025-03-21 16:41:55 +0700

    Reapply "change frontend and cpanel base"
    This reverts commit 2433a23258851c72ced170feb3520d056b3de34f.

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,7 @@ import fs from 'fs'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   optimizeDeps: {
     include: ['@emotion/styled', '@emotion/react'],
   },
