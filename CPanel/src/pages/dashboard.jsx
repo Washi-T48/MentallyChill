398293b8a556856d950644db49ad35cd3f1dc831
martksk <kritsakorn.lekd@kmutt.ac.th>
2025-04-26 00:39:32 +0700

    Update dashboard.jsx

--- CPanel/src/pages/dashboard.jsx
+++ CPanel/src/pages/dashboard.jsx
@@ -441,9 +441,9 @@ export default function DashboardPage() {
                 series={[
                   {
                     data: [
-                      { id: 0, value: 10, label: 'series A' },
-                      { id: 1, value: 15, label: 'series B' },
-                      { id: 2, value: 20, label: 'series C' },
+                      { id: 0, value: 10, label: 'ระดับรุนแรง' },
+                      { id: 1, value: 15, label: 'ระดับปานกลาง' },
+                      { id: 2, value: 20, label: 'ระดับปกติ' },
                     ],
                     innerRadius: 70,
                     outerRadius: 150,
