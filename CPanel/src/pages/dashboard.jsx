a26e0804f96ca6a397842ea789718522aff1feae
martksk <kritsakorn.lekd@kmutt.ac.th>
2025-04-25 23:35:39 +0700

    Update dashboard.jsx

--- CPanel/src/pages/dashboard.jsx
+++ CPanel/src/pages/dashboard.jsx
@@ -404,7 +404,6 @@ export default function DashboardPage() {
         </div>
         <div className="w-full overflow-x-hidden">
           <div className="p-4 md:p-10">
-            <div>สวัสดี {username}</div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
               <StatCard
                 title="คำขอการจอง"
