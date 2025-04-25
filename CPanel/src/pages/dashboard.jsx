cf4bd2f678cd5a2791076d251ac9e1d12db95bef
martksk <kritsakorn.lekd@kmutt.ac.th>
2025-04-26 00:28:31 +0700

    Update dashboard.jsx

--- CPanel/src/pages/dashboard.jsx
+++ CPanel/src/pages/dashboard.jsx
@@ -458,7 +458,7 @@ export default function DashboardPage() {
                 width={300}
                 height={300}
               />
-              <div>
+              <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4 mb-8">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
                   <StatCard title="ระดับปกติ" value={lowCount} color="green" />
                   <StatCard
