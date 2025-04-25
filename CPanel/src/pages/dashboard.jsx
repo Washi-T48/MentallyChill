85f3fe8193e8972a4219a6700ccec241b89e5463
martksk <kritsakorn.lekd@kmutt.ac.th>
2025-04-26 00:49:10 +0700

    Update dashboard.jsx

--- CPanel/src/pages/dashboard.jsx
+++ CPanel/src/pages/dashboard.jsx
@@ -442,7 +442,11 @@ export default function DashboardPage() {
                     arcLabel: (item) => `${item.value}%`,
                     arcLabelMinAngle: 35,
                     arcLabelRadius: '60%',
-                    data: [lowCount, mediumCount, highCount],
+                    data: [
+                      { id: 0, value: 10, label: 'series A' },
+                      { id: 1, value: 15, label: 'series B' },
+                      { id: 2, value: 20, label: 'series C' },
+                    ],
                   },
                 ]}
                 sx={{
