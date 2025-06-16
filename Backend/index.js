eb72be285ac441dd2a9772169b03bbd1e59ce8ae
martksk <krtsk2547@gmail.com>
2025-06-16 14:01:50 +0700

    add CORS

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser';
-import https from 'https';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import https from "https";
+import cors from "cors";
 
-import publicRouter from './Routes/publicRoutes.js';
-import logRouter from './Routes/logRoutes.js';
-import lineRouter from './Routes/lineRoutes.js';
-import authRouter from './Routes/authRoutes.js';
-import userRouter from './Routes/userRoutes.js';
-import staffRouter from './Routes/staffRoutes.js';
-import formsRouter from './Routes/formsRoutes.js';
-import appointmentRouter from './Routes/appointmentRoutes.js';
-import timetableRouter from './Routes/timetableRoutes.js'
-import exportRouter from './Routes/exportRoutes.js';
+import publicRouter from "./Routes/publicRoutes.js";
+import logRouter from "./Routes/logRoutes.js";
+import lineRouter from "./Routes/lineRoutes.js";
+import authRouter from "./Routes/authRoutes.js";
+import userRouter from "./Routes/userRoutes.js";
+import staffRouter from "./Routes/staffRoutes.js";
+import formsRouter from "./Routes/formsRoutes.js";
+import appointmentRouter from "./Routes/appointmentRoutes.js";
+import timetableRouter from "./Routes/timetableRoutes.js";
+import exportRouter from "./Routes/exportRoutes.js";
 
-import logger, { consoleLogExpress } from './Middleware/logger.js';
-import fs from 'fs';
+import logger, { consoleLogExpress } from "./Middleware/logger.js";
+import fs from "fs";
 
 const app = express();
 
@@ -24,17 +24,18 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: [
-        'http://localhost',
-        'http://localhost:80',
-        'http://localhost:3000',
-        'https://mindcra.com',
-        'https://www.mindcra.com',
-        'https://mindcra.com:444',
-        'https://www.mindcra.com:444',
-    ],
-    credentials: true,
-}
+  origin: [
+    "http://localhost",
+    "http://localhost:80",
+    "http://localhost:3000",
+    "https://mindcra.com",
+    "https://www.mindcra.com",
+    "https://mindcra.com:444",
+    "https://www.mindcra.com:3000",
+    "http://localhost:444",
+  ],
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -43,9 +44,9 @@ app.use(consoleLogExpress);
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", publicRouter);
-app.use("/log", logRouter)
+app.use("/log", logRouter);
 app.use("/line", lineRouter);
-app.use("/auth", authRouter)
+app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/staff", staffRouter);
 app.use("/forms", formsRouter);
@@ -53,10 +54,17 @@ app.use("/appointment", appointmentRouter);
 app.use("/timetable", timetableRouter);
 app.use("/export", exportRouter);
 
-https.createServer({
-    key: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/privkey.pem`),
-    cert: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/cert.pem`),
-    ca: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/chain.pem`),
-}, app).listen(PORT, () => {
+https
+  .createServer(
+    {
+      key: fs.readFileSync(
+        `/etc/letsencrypt/live/mindcra.com-0001/privkey.pem`
+      ),
+      cert: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/cert.pem`),
+      ca: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/chain.pem`),
+    },
+    app
+  )
+  .listen(PORT, () => {
     logger.info(`Server started on port ${PORT}`);
-});
+  });
--- CPanel/vite.config.js
+++ CPanel/vite.config.js
@@ -1,30 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from 'fs'
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 444,
-    https: {
-      key: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/privkey.pem`),
-      cert: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/cert.pem`),
-      ca: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/chain.pem`),
-    },
+    https: false,
+    //    https: {
+    //      key: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/privkey.pem`),
+    //      cert: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/cert.pem`),
+    //      ca: fs.readFileSync(`/etc/letsencrypt/live/mindcra.com-0001/chain.pem`),
+    //    },
     proxy: {
-      '/api': {
-        target: 'https://sombat.cc:3000/',
+      "/api": {
+        target: "https://sombat.cc:3000/",
         changeOrigin: true,
         secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
-    }
+    },
   },
   build: {
-    outDir: 'var/www/cpanel',
+    outDir: "var/www/cpanel",
     emptyOutDir: true,
   },
 });
